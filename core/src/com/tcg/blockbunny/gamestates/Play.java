b5b53b1ed1e64e1e6cb11f7f814712d1458e3f17
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2018-05-30 19:48:36 -0700

    V5 Load in Map and Create Bodies for Red Layerr

--- core/src/com/tcg/blockbunny/gamestates/Play.java
+++ core/src/com/tcg/blockbunny/gamestates/Play.java
@@ -1,6 +1,10 @@
 package com.tcg.blockbunny.gamestates;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.tcg.blockbunny.Game;
@@ -21,6 +25,11 @@ public class Play extends GameState {
     private Body playerBody;
     private MyContactListener cl;
 
+    private TiledMap tileMap;
+    private OrthogonalTiledMapRenderer tmr;
+
+    private float tileSize;
+
     public Play(GameStateManager gsm) {
         super(gsm);
 
@@ -31,22 +40,8 @@ public class Play extends GameState {
 
         // create platform
         BodyDef bdef = new BodyDef();
-        bdef.position.set(160 / PPM, 120 / PPM);
-        bdef.type = BodyDef.BodyType.StaticBody;
-        // static body - don't move, unaffected by forces
-        // kinematic body - don't get affected forces, can have velocity
-        // dynamic body - always get affected by forces
-
-        Body body = world.createBody(bdef);
-
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(50 / PPM, 5 / PPM);
-
         FixtureDef fdef = new FixtureDef();
-        fdef.shape = shape;
-        fdef.filter.categoryBits = B2DVars.BIT_GROUND;
-        fdef.filter.maskBits = B2DVars.BIT_PLAYER;
-        body.createFixture(fdef).setUserData("ground");
 
         // create player
         bdef.position.set(160 / PPM, 200 / PPM);
@@ -56,7 +51,7 @@ public class Play extends GameState {
         shape.setAsBox(5 / PPM, 5 / PPM);
         fdef.shape = shape;
         fdef.filter.categoryBits = B2DVars.BIT_PLAYER;
-        fdef.filter.maskBits = B2DVars.BIT_GROUND;
+        fdef.filter.maskBits = B2DVars.BIT_RED;
         playerBody.createFixture(fdef).setUserData("player");
 
         // create foot sensor
@@ -64,12 +59,54 @@ public class Play extends GameState {
         fdef.shape = shape;
         fdef.isSensor = true;
         fdef.filter.categoryBits = B2DVars.BIT_PLAYER;
-        fdef.filter.maskBits = B2DVars.BIT_GROUND;
+        fdef.filter.maskBits = B2DVars.BIT_RED;
         playerBody.createFixture(fdef).setUserData("foot");
 
         //set up box2d cam
         b2dCam = new OrthographicCamera();
         b2dCam.setToOrtho(false, Game.V_WIDTH / PPM, Game.V_HEIGHT / PPM);
+
+        ////////////////////////////////////////////////////
+
+        // load tile map
+        tileMap = new TmxMapLoader().load("res/maps/test.tmx");
+        tmr = new OrthogonalTiledMapRenderer(tileMap);
+
+        TiledMapTileLayer layer = (TiledMapTileLayer) tileMap.getLayers().get("red");
+        tileSize = layer.getTileWidth();
+
+        // go through all cells in layer
+        for (int row = 0; row < layer.getHeight(); row++) {
+            for (int col = 0; col < layer.getWidth(); col++) {
+                // get cell
+                TiledMapTileLayer.Cell cell = layer.getCell(col, row);
+                // check if cell exists
+                if(cell == null) continue;
+                if(cell.getTile() == null) continue;
+                //create a body and a fixture from cell
+                BodyDef cellBodyDef = new BodyDef();
+                cellBodyDef.type = BodyDef.BodyType.StaticBody;
+                cellBodyDef.position.set(((col + .5f) * tileSize) / PPM, ((row + .5f) * tileSize) / PPM);
+
+
+                ChainShape cs = new ChainShape();
+                Vector2[] v = new Vector2[] {
+                    new Vector2(-tileSize / 2 / PPM, -tileSize / 2 / PPM), // bottom left
+                    new Vector2(-tileSize / 2 / PPM, tileSize / 2 / PPM), // top left
+                    new Vector2(tileSize / 2 / PPM, tileSize / 2 / PPM), // top right
+                    new Vector2(tileSize / 2 / PPM, -tileSize / 2 / PPM), // bottom right
+                };
+
+                cs.createChain(v);
+                FixtureDef cellFixtureDef = new FixtureDef();
+                cellFixtureDef.friction = 0;
+                cellFixtureDef.shape = cs;
+                cellFixtureDef.filter.categoryBits = B2DVars.BIT_RED;
+                cellFixtureDef.filter.maskBits = B2DVars.BIT_PLAYER;
+                cellFixtureDef.isSensor = false;
+                world.createBody(cellBodyDef).createFixture(cellFixtureDef);
+            }
+        }
 
     }
 
@@ -91,6 +128,8 @@ public class Play extends GameState {
 
     @Override
     public void render() {
+        tmr.setView(cam);
+        tmr.render();
         b2dr.render(world, b2dCam.combined);
     }
 
--- core/src/com/tcg/blockbunny/managers/B2DVars.java
+++ core/src/com/tcg/blockbunny/managers/B2DVars.java
@@ -5,7 +5,9 @@ public class B2DVars {
     public static final float PPM = 100f; // pixels per meter
 
     // category bits
-    public static final short BIT_GROUND = 1 << 1;
-    public static final short BIT_PLAYER = 1 << 2;
+    public static final short BIT_PLAYER = 1 << 1;
+    public static final short BIT_RED = 1 << 2;
+    public static final short BIT_GREEN = 1 << 3;
+    public static final short BIT_BLUE = 1 << 4;
 
 }
